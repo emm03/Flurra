59b19af1fb56ecd2403719db86b7f7dcfe1059b5
emm03 <117182644+emm03@users.noreply.github.com>
2026-09-04 18:09:25 -0700

    Load Feather icons globally

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,11 @@
+import { Feather } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
+  const [iconsLoaded] = useFonts(Feather.font);
+  if (!iconsLoaded) return null;
+
   return <><StatusBar style="light" /><Stack screenOptions={{ headerShown: false }} /></>;
 }
